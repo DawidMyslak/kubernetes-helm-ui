ff6c60383bfb3a640c282664606c32a00522f62c
DawidMyslak <dawid.myslak@teamwork.com>
2017-07-13 09:57:49 +0100

    ui improvements

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -3,21 +3,28 @@
     <div class="menu">
       <ul>
         <li>
-          <router-link to="/releases">Releases</router-link>
+          <router-link to="/releases">
+            <img src="~@/assets/releases.svg" class="icon">
+            <span class="title">Releases</span>
+          </router-link>
         </li>
         <li>
           <router-link to="/history">
-            <img src="~@/assets/history.svg" width="20" height="20">
-            <br>History
+            <img src="~@/assets/history.svg" class="icon">
+            <span class="title">History</span>
           </router-link>
         </li>
         <li>
           <router-link to="/settings">
-            <img src="~@/assets/settings.svg" width="20" height="20">
-            <br>Settings</router-link>
+            <img src="~@/assets/settings.svg" class="icon">
+            <span class="title">Settings</span>
+          </router-link>
         </li>
         <li>
-          <router-link to="/console">Console</router-link>
+          <router-link to="/console">
+            <img src="~@/assets/console.svg" class="icon">
+            <span class="title">Console</span>
+          </router-link>
         </li>
       </ul>
     </div>
@@ -52,7 +59,7 @@ body {
 .menu {
   width: 80px;
   height: 100vh;
-  background-color: #ccc;
+  background-color: #3bacdf;
   position: absolute;
 }
 
@@ -61,23 +68,34 @@ body {
 }
 
 .menu li a {
-  width: 100%;
+  width: 80px;
+  height: 80px;
   display: block;
   text-align: center;
   text-decoration: none;
   font-size: 12px;
   color: #fff;
-  padding: 10px 0;
+  padding: 20px 0;
 }
 
 .menu li a:hover {
-  background-color: #aaa;
+  background-color: #3193c0;
+}
+
+.icon {
+  width: 20px;
+  height: 20px;
+}
+
+.title {
+  width: 80px;
+  display: block;
 }
 
 .content {
   width: 100vw;
   height: 100vh;
-  background-color: #eee;
+  background-color: #fff;
   padding-left: 80px;
   overflow: scroll;
 }
